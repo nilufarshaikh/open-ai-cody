1b0f9ed49782e76900cf5f44951a41297b9a5ffd
Nilufar Shaikh <nilufarrehanshaikh@gmail.com>
2024-11-28 01:51:54 -0500

    Added openAI apis to server and connected with client

--- client/src/script.js
+++ client/src/script.js
@@ -54,3 +54,55 @@ const chatStripe = (isAi, value, uniqueId) => {
     </div>
     `;
 };
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  const data = new FormData(form);
+
+  chatContainer.innerHTML += chatStripe(false, data.get("prompt"));
+
+  form.reset();
+
+  const uniqueId = generateUniqueId();
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
+
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+
+  const messageDiv = document.getElementById(uniqueId);
+
+  loader(messageDiv);
+
+  const response = await fetch("http://localhost:5000", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      prompt: data.get("prompt"),
+    }),
+  });
+
+  clearInterval(loadInterval);
+  messageDiv.innerHTML = "";
+
+  if (response.ok) {
+    const data = await response.json();
+    const parsedData = data.bot.trim();
+
+    typeText(messageDiv, parsedData);
+  } else {
+    const err = await response.text();
+
+    messageDiv.innerHTML = "Something went wrong.";
+
+    alert(err);
+  }
+};
+
+form.addEventListener("submit", handleSubmit);
+form.addEventListener("keyup", (e) => {
+  if (e.keyCode === 13) {
+    handleSubmit(e);
+  }
+});
---
+++ server/server.js
@@ -0,0 +1,69 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import OpenAI from "openai";
+
+dotenv.config();
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  res.status(200).send({
+    message: "Hello from Cody!",
+  });
+});
+
+app.post("/", async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+
+    const response = await openai.chat.completions.create({
+      model: "gpt-4o-mini",
+      messages: [
+        {
+          role: "system",
+          content: [
+            {
+              type: "text",
+              text: "You are an expert programming assistant who helps write code, debug and improve code.",
+            },
+          ],
+        },
+        {
+          role: "user",
+          content: [
+            {
+              type: "text",
+              text: prompt,
+            },
+          ],
+        },
+      ],
+      response_format: {
+        type: "text",
+      },
+      temperature: 0,
+      max_tokens: 500,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
+    });
+
+    res.status(200).send({
+      bot: response.choices[0].message.content,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error });
+  }
+});
+
+app.listen(5000, () =>
+  console.log("Server is running on http://localhost:5000")
+);
